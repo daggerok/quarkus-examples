11b7616d0c4a388085128cae103c30e43e1a0c82
Maksim Kostromin <daggerok@gmail.com>
2019-09-14 19:07:49 +0300

    Ugh. Bad rebase. (C) whatthecommit.com

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,32 @@
 import com.avast.gradle.dockercompose.RemoveImages
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
+object Versions {
+    const val vavr = "0.10.2"
+    const val gradle = "5.6.2"
+    const val quarkus = "0.21.2"
+    const val versionsPlugin = "0.25.0"
+    const val dockerComposePlugin = "0.9.4"
+}
+
 buildscript {
     repositories {
         mavenCentral()
     }
     dependencies {
-        classpath("io.quarkus:quarkus-gradle-plugin:0.21.2")
+        classpath("io.quarkus:quarkus-gradle-plugin:${Versions.quarkus}")
     }
 }
 
 plugins {
     idea
     java
-    id("com.github.ben-manes.versions") version "0.25.0" apply false
-    id("com.avast.gradle.docker-compose") version "0.9.4" apply false
+    id("com.avast.gradle.docker-compose") version Versions.dockerComposePlugin apply false
+    id("com.github.ben-manes.versions") version Versions.versionsPlugin apply false
     // // for some reasons, test wont work with new style quarkus plugin declaration:
     // https://github.com/quarkusio/quarkus/issues/3552#issuecomment-524225607
-    // id("io.quarkus") version "0.21.2" apply false
+    // id("io.quarkus") version Versions.quarkus apply false
 }
-
-apply<io.quarkus.gradle.QuarkusPlugin>()
 
 allprojects {
     version = "0.0.2-SNAPSHOT"
@@ -41,9 +47,11 @@ subprojects {
         //maven(url = uri("https://repository.jboss.org/nexus/content/groups/public"))
     }
 
+    apply<io.quarkus.gradle.QuarkusPlugin>()
+
     dependencies {
-        implementation(enforcedPlatform("io.quarkus:quarkus-bom:0.21.2"))
-        implementation(enforcedPlatform("io.vavr:vavr:0.10.2"))
+        implementation(enforcedPlatform("io.quarkus:quarkus-bom:${Versions.quarkus}"))
+        implementation(enforcedPlatform("io.vavr:vavr:${Versions.vavr}"))
     }
 
     tasks {
@@ -61,16 +69,12 @@ subprojects {
                 )
             }
         }
-        withType<Wrapper> {
-            gradleVersion = "5.6.2"
-            distributionType = Wrapper.DistributionType.BIN
-        }
     }
 }
 
 tasks {
     withType<Wrapper> {
-        gradleVersion = "5.6.2"
+        gradleVersion = Versions.gradle
         distributionType = Wrapper.DistributionType.BIN
     }
 }
