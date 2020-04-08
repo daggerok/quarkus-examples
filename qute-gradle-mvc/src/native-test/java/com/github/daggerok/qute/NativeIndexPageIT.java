127d7f2c916879669b8198ef79eeba8d9c58043b
Maksim Kostromin <daggerok@gmail.com>
2020-04-08 21:31:40 +0300

    workaround for ant being a pile of fail (C) whatthecommit.com

---
+++ qute-gradle-mvc/src/native-test/java/com/github/daggerok/qute/NativeIndexPageIT.java
@@ -0,0 +1,9 @@
+package com.github.daggerok.qute;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeIndexPageIT extends IndexPageTest {
+
+    // Execute the same tests but in native mode.
+}
